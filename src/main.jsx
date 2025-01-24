2d4d831d68951e0a2fae64bb56c2679103f19adb
Abdul Salam <92994347+abdulsalamshah@users.noreply.github.com>
2025-01-25 01:01:03 +0500

    commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import { Analytics } from '@vercel/analytics/react';
 import App from './App';
 import './index.css';
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
-      <Analytics />
     </BrowserRouter>
   </React.StrictMode>
 );
